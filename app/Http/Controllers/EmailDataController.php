c355b1ee8a6ca61b38b85cc37d78eb0b51d65e0b
root <root@62.171.133.55 buboff.com>
2021-02-05 14:40:41 +0100

    Added h1 tag

--- app/Http/Controllers/EmailDataController.php
+++ app/Http/Controllers/EmailDataController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EmailData;
+use Illuminate\Support\Facades\DB;
 
 class EmailDataController extends Controller
 {
     public function track(Request $request){
 
 
-$validated=$request->get('code');
-if($validated!==null){
+$validated[]=$request->get('code');
+$validated[]=$request->get('name');
+if($validated[0]=='open'&$validated[1]!==null){
+
+$emaildata= new EmailData();
+$countOpen=DB::table('email_data')->get('countOpen');
+$countOpen=$countOpen[0]->countOpen;
+$countOpen++;
 var_dump ($validated);
+var_dump ($countOpen);
 echo date(DATE_ATOM, time());
 }else{
 echo "Nothing is there";
